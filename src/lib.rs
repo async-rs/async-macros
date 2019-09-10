6dda5b6856fb7b5742ad68113ba80d6dba304eaa
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-09-11 01:38:42 +0200

    top-level example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
 //! Macros for async-std.
+//!
+//! # Examples
+//!
+//! ```
+//! #![feature(async_await)]
+//! # futures::executor::block_on(async {
+//! use async_macros::join;
+//! use futures::future;
+//!
+//! let a = future::ready(1u8);
+//! let b = future::ready(2u8);
+//!
+//! assert_eq!(join!(a, b).await, (1, 2));
+//! # });
+//! ```
 
 #![forbid(future_incompatible, rust_2018_idioms)]
 #![deny(missing_debug_implementations, nonstandard_style)]
