14f3122d05f92cee3ab2c42d34e178d39408d0b3
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-09-11 00:32:18 +0200

    init with ready
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Macros for async-std.
+
+#![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
+#![cfg_attr(test, deny(warnings))]
+
+mod ready;
